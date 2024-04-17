0a35ba0a3fe228e5faa51dc36f92b2a4f81bc3ac
fcj <3147076912@qq.com>
2024-04-17 09:20:48 +0800

    AddressBookManagement

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.addressmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires pinyin4j;
+
+    opens org.example.addressmanagement to javafx.fxml;
+    exports org.example.addressmanagement;
+}
