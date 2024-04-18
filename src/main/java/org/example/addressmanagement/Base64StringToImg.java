c43423b3c442cbb1ae5fcd65132b27511fd45658
Nexus <3058597506@qq.com>
2024-04-18 15:51:24 +0800

    创建了在图片文件和Base64加密字符串之间转换的工具类

--- src/main/java/org/example/addressmanagement/Base64StringToImg.java
+++ src/main/java/org/example/addressmanagement/Base64StringToImg.java
@@ -1,11 +1,11 @@
-package org.example;
+package org.example.addressmanagement;
 
 import java.io.*;
 import java.util.Base64;
 
 public class Base64StringToImg {
 public static String ImgtoString(String imgFile) throws IOException {//将图片文件转化为字节数组字符串，并对其进行Base64编码处理
-    //"D:\\desktop\\量子计算.jpg"
+    //"D:\\desktop\\test.jpg"
     InputStream in = null;
     byte[] data = null;
     //读取图片字节数组
@@ -22,14 +22,13 @@ public static String ImgtoString(String imgFile) throws IOException {//将图片
     }
     return Base64.getEncoder().encodeToString(data);
 }
-    public static String Toimg(String encoded) throws IOException {
-        String filename="D:\\desktop\\result.jpg";
+    public static void StringToimg(String encoded,String filename) throws IOException {
+        //String filename="D:\\desktop\\result.jpg";
         File file=new File(filename);
         file.createNewFile();
         FileOutputStream fos=new FileOutputStream(file);
         fos.write(decode(encoded));
         fos.close();
-        return encoded;
     }
     public static byte[] decode(String encoded)
     {
